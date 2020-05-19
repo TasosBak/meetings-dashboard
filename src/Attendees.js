afa226d6b7cbb4ed2b2a243b5f55ea7787ab8cc2
Vakirtzis Anastasios <anastasiosvakirtzis@gmail.com>
2020-05-19 17:17:27 +0300

    Added AttendeesList page and a button to view the list

--- src/Attendees.js
+++ src/Attendees.js
@@ -1,35 +1,32 @@
-import React, { Component } from 'react';
-import firebase from './Firebase';
+import React, { Component } from "react";
+import firebase from "./Firebase";
+import AttendeesList from "./AttendeesList";
 
 class Attendees extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      displayAttendees: []
+      displayAttendees: [],
     };
   }
 
   componentDidMount() {
     const ref = firebase
       .database()
-      .ref(
-        `meetings/${this.props.userID}/${
-          this.props.meetingID
-        }/attendees`
-      );
+      .ref(`meetings/${this.props.userID}/${this.props.meetingID}/attendees`);
 
-    ref.on('value', snapshot => {
+    ref.on("value", (snapshot) => {
       let attendees = snapshot.val();
       let attendeesList = [];
       for (let item in attendees) {
         attendeesList.push({
           attendeeID: item,
           attendeeName: attendees[item].attendeeName,
-          attendeeEmail: attendees[item].attendeeEmail
+          attendeeEmail: attendees[item].attendeeEmail,
         });
       }
       this.setState({
-        displayAttendees: attendeesList
+        displayAttendees: attendeesList,
       });
     });
   }
@@ -39,12 +36,13 @@ class Attendees extends Component {
       <div className="container mt-4">
         <div className="row justify-content-center">
           <div className="col-md-8">
-            <h1 className="font-weight-light text-center">
-              Attendees
-            </h1>
+            <h1 className="font-weight-light text-center">Attendees</h1>
           </div>
         </div>
-        List Goes Here
+        <AttendeesList
+          userID={this.props.userID}
+          attendees={this.state.displayAttendees}
+        />
       </div>
     );
   }
