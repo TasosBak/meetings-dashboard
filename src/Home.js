4e4fb2df0c56e54231e30c49e280d9e798401d1a
Vakirtzis Anastasios <anastasiosvakirtzis@gmail.com>
2020-05-19 20:04:37 +0300

    Final changes

--- src/Home.js
+++ src/Home.js
@@ -23,11 +23,12 @@ class Home extends Component {
               Meeting Log
             </div>
             <p className="lead" style={biggerLead}>
-              This simple app creates meetings, allows people to check in, and
-              picks random users to award giveaways. It's a good example of a
-              Single Page Application which includes connection to a database
-              and routing. It's a practical way to learn{" "}
-              <a href="https://reactjs.org/">React</a> with{" "}
+              Using this app you can create meetings where people can check in
+              and pick random user (e.g. to award giveways). The admin only can
+              delete any attendee, the simple users can only see the list of
+              attendees. The project includes routing which is achieved with
+              reach router and connection to a database. It's a nice boilerplate
+              of using <a href="https://reactjs.org/">React</a> with{" "}
               <a href="https://firebase.google.com">Firebase</a>.
             </p>
 
