2e0065b1bf40dd8233a4485045a727ee513c94ef
Vakirtzis Anastasios <anastasiosvakirtzis@gmail.com>
2020-05-19 13:31:54 +0300

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+import Home from "./Home";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      user: null,
+    };
+  }
+
+  render() {
+    return <Home user={this.state.user} />;
+  }
+}
+
+export default App;
