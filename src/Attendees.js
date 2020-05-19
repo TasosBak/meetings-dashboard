d316980e6ee2390101c76509dee83ddf239e1f54
Vakirtzis Anastasios <anastasiosvakirtzis@gmail.com>
2020-05-19 19:41:59 +0300

    Added random user button

--- src/Attendees.js
+++ src/Attendees.js
@@ -1,18 +1,20 @@
 import React, { Component } from "react";
 import firebase from "./Firebase";
 import AttendeesList from "./AttendeesList";
-import { FaUndo } from "react-icons/fa";
+import { FaUndo, FaRandom } from "react-icons/fa";
 
 class Attendees extends Component {
   constructor(props) {
     super(props);
     this.state = {
       searchQuery: "",
+      allAttendees: [],
       displayAttendees: [],
     };
 
     this.handleChange = this.handleChange.bind(this);
     this.resetQuery = this.resetQuery.bind(this);
+    this.chooseRandom = this.chooseRandom.bind(this);
   }
 
   componentDidMount() {
@@ -32,6 +34,7 @@ class Attendees extends Component {
         });
       }
       this.setState({
+        allAttendees: attendeesList,
         displayAttendees: attendeesList,
       });
     });
@@ -44,8 +47,19 @@ class Attendees extends Component {
     this.setState({ [itemName]: itemValue });
   }
 
+  chooseRandom() {
+    const randomAttendee = Math.floor(
+      Math.random() * this.state.allAttendees.length
+    );
+    this.resetQuery();
+    this.setState({
+      displayAttendees: [this.state.allAttendees[randomAttendee]],
+    });
+  }
+
   resetQuery() {
     this.setState({
+      displayAttendees: this.state.allAttendees,
       searchQuery: "",
     });
   }
@@ -75,6 +89,13 @@ class Attendees extends Component {
                     onChange={this.handleChange}
                   />
                   <div className="input-group-append">
+                    <button
+                      className="btn btn-sm btn-outline-info "
+                      title="Pick a random attendee"
+                      onClick={() => this.chooseRandom()}
+                    >
+                      <FaRandom />
+                    </button>
                     <button
                       className="btn btn-sm btn-outline-info "
                       title="Reset Search"
