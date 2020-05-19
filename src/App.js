a4df2a2dded798d20de8365f20c4ae7c6d647e2e
Vakirtzis Anastasios <anastasiosvakirtzis@gmail.com>
2020-05-19 15:20:07 +0300

    Finishing up with registration. Redirecting after user is registered.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // Import React
 import React, { Component } from "react";
-import { Router } from "@reach/router";
+import { Router, navigate } from "@reach/router";
 import firebase from "./Firebase";
 
 import Home from "./Home";
@@ -15,6 +15,8 @@ class App extends Component {
     super();
     this.state = {
       user: null,
+      displayName: null,
+      userID: null,
     };
   }
 
@@ -28,17 +30,32 @@ class App extends Component {
     });
   }
 
+  registerUser = (userName) => {
+    firebase.auth().onAuthStateChanged((FBUser) => {
+      FBUser.updateProfile({
+        displayName: userName,
+      }).then(() => {
+        this.setState({
+          user: FBUser,
+          displayName: FBUser.displayName,
+          userID: FBUser.uid,
+        });
+        navigate("/meetings");
+      });
+    });
+  };
+
   render() {
     return (
       <div>
         <Navigation user={this.state.user} />
-        {this.state.user && <Welcome user={this.state.user} />}
+        {this.state.user && <Welcome user={this.state.displayName} />}
 
         <Router>
           <Home path="/" user={this.state.user} />
           <Login path="/login" />
           <Meetings path="/meetings" />
-          <Register path="/register" />
+          <Register path="/register" registerUser={this.registerUser} />
         </Router>
       </div>
     );
